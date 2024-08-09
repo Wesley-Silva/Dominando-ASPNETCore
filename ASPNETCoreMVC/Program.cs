2af7b5ddd33a34ad896dbde1228f7e2e8f157151
Wesley-Silva <wesleybf3@gmail.com>
2024-08-09 15:19:39 -0300

    Injeção de dependência

--- ASPNETCoreMVC/Program.cs
+++ ASPNETCoreMVC/Program.cs
@@ -1,5 +1,7 @@
 using ASPNETCoreMVC.Data;
 using ASPNETCoreMVC.Extensions;
+using ASPNETCoreMVC.Services;
+using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +12,23 @@ builder.Services.AddControllersWithViews();
 //builder.Services.AddRouting(options =>
 //    options.ConstraintMap["slugify"] = typeof(RouteSlugifyParameterTransformer));
 
+// Adicionado suporte a mudança de convenção da rota de areas
+builder.Services.Configure<RazorViewEngineOptions>(options =>
+{
+    options.AreaViewLocationFormats.Clear();
+    options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/{1}/{0}.cshtml");
+    options.AreaViewLocationFormats.Add("/Modulos/{2}/Views/Shared/{0}.cshtml");
+    options.AreaViewLocationFormats.Add("/Views/Shared/{0}.cshtml");
+});
+
 builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+builder.Services.AddTransient<IOperacaoTransient, Operacao>();
+builder.Services.AddScoped<IOperacaoScoped, Operacao>();
+builder.Services.AddSingleton<IOperacaoSingleton, Operacao>();
+builder.Services.AddSingleton<IOperacaoSingletonInstance>(new Operacao(Guid.Empty));
+
+builder.Services.AddTransient<OperacaoService>();
 
 builder.Services.AddDbContext<AppDbContext>(o => 
     o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -37,8 +55,26 @@ app.UseAuthorization();
 //    name: "default",
 //    pattern: "{controller:slugify=Home}/{action:slugify=Index}/{id?}");
 
+//app.MapControllerRoute(
+//    name: "areas",
+//    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+// Rota de área especializada
+app.MapAreaControllerRoute("AreaProdutos", "Produtos", "Produtos/{controller=Cadastro}/{action=Index}/{id?}");
+app.MapAreaControllerRoute("AreaVendas", "Vendas", "Vendas/{controller=Gestao}/{action=Index}/{id?}");
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Passando uma instancia antes da aplicação start
+using (var scopeService = app.Services.CreateScope())
+{
+    var services = scopeService.ServiceProvider;
+
+    var singService = services.GetRequiredService<IOperacaoSingleton>();
+
+    Console.WriteLine("Direto da Program.cs" + singService.OperacaoId);
+}
 
 app.Run();
---
+++ ASPNETCoreMVC/Services/Operacao.cs
@@ -0,0 +1,60 @@
+﻿namespace ASPNETCoreMVC.Services
+{
+    public class OperacaoService
+    {
+        public OperacaoService(IOperacaoTransient transiente, IOperacaoScoped scoped,
+                               IOperacaoSingleton singleton, IOperacaoSingletonInstance singletonInstance)
+        {
+            Transiente = transiente;
+            Scoped = scoped;
+            Singleton = singleton;
+            SingletonInstance = singletonInstance;
+        }
+
+        public IOperacaoTransient Transiente { get; set; }
+        public IOperacaoScoped Scoped { get; set; }
+        public IOperacaoSingleton Singleton { get; set; }
+        public IOperacaoSingletonInstance SingletonInstance { get; set; }
+    }
+
+    public class Operacao : IOperacaoTransient, IOperacaoScoped,
+                            IOperacaoSingleton, IOperacaoSingletonInstance
+    {
+        public Operacao() : this(Guid.NewGuid())
+        {
+            
+        }
+
+        public Operacao(Guid id)
+        {
+            OperacaoId = id;
+        }
+
+        public Guid OperacaoId { get; private set; }
+    }
+
+    public interface IOperacao
+    {
+        Guid OperacaoId { get; }
+    }
+
+    public interface IOperacaoTransient : IOperacao
+    {
+        
+    }
+
+    public interface IOperacaoScoped : IOperacao
+    {
+        
+    }
+
+    public interface IOperacaoSingleton : IOperacao
+    {
+        
+    }
+
+    public interface IOperacaoSingletonInstance : IOperacao
+    {
+        
+    }
+}
